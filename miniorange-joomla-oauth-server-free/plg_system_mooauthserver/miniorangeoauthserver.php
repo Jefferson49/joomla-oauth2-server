66ef6975255692875e12b87e9c49ef2c557ecc3d
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2024-08-24 08:36:18 +0200

    Short term patch: Removed restricted access

--- miniorange-joomla-oauth-server-free/plg_system_mooauthserver/miniorangeoauthserver.php
+++ miniorange-joomla-oauth-server-free/plg_system_mooauthserver/miniorangeoauthserver.php
@@ -7,7 +7,7 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 // no direct access
-defined( '_JEXEC' ) or die( 'Restricted access' );
+//defined( '_JEXEC' ) or die( 'Restricted access' );
 define('_JEXEC', 1);
 define('JPATH_BASE','../../..' );
 require_once JPATH_BASE . '/includes/defines.php';
